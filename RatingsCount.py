748e33820ad39530348a7bbdd043f49558a0bd8d
haleyhee <63924462+haleyhee@users.noreply.github.com>
2021-02-27 20:07:46 -0500

    Update RatingsCount.py

--- RatingsCount.py
+++ RatingsCount.py
@@ -5,11 +5,12 @@ class RatingsCount(MRJob):
     def steps(self):
         return [MRStep(mapper=self.mapper_get_ratings,
                       reducer=self.reducer_count_ratings), 
-               MRStep (reducer=self.reducer_sorted_output)]
+                MRStep (reducer=self.reducer_sorted_output)
+               ]
     
     def mapper_get_ratings(self, _, line):
         (userID, movieID, rating, timestamp) = line.split('\t')
-        yield movieID, 1
+        yield movieID, 1 
         
     def reducer_count_ratings(self, key, values):
         yield str(sum(values)).zfill(5), key
